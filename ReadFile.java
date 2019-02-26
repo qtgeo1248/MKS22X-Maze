922816c6c2d47e39c444c2f4f35c4dc73e96cfad
qtgeo1248 <gzhou00@stuy.edu>
2019-02-26 11:39:42 -0500

    counted num of cols now testing

--- ReadFile.java
+++ ReadFile.java
@@ -8,6 +8,7 @@ public class ReadFile {
             File f = new File("Maze1.txt");
             Scanner in = new Scanner(f);
             int numRows = 0;
+            int numCols = 0;
 
             while (in.hasNextLine()) { //this prints out each line
                 String line = in.nextLine();
@@ -15,7 +16,7 @@ public class ReadFile {
                 numRows++; //keeps track of num of rows for array of chars
                 if (numRows == 1) { //counts the number of columns
                     for (int i = 0; i < line.length(); i++) {
-                        
+                        numCols++;
                     }
                 }
             }
