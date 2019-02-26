1c42505434ff1d875786801387f0166a5ca0d3c5
qtgeo1248 <gzhou00@stuy.edu>
2019-02-26 11:35:55 -0500

    set up the file setup and excetpions

--- ReadFile.java
+++ ReadFile.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) {
-
+        try {
+            File f = new File("Maze1.txt");
+            Scanner in = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("NO FILE");
+        }
     }
 }
