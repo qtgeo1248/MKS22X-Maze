83a6f2899d067300e5e44ec0d3ef1a222d96baee
qtgeo1248 <gzhou00@stuy.edu>
2019-02-27 11:09:13 -0500

    copy pasted my row and col counter in readfile.java

--- Maze.java
+++ Maze.java
@@ -6,6 +6,19 @@ public class Maze {
     private boolean animate;
 
     public Maze(String filename) throws FileNotFoundException {
-        
+        File f = new File("Maze1.txt");
+        Scanner in = new Scanner(f);
+        int numRows = 0;
+        int numCols = 0;
+
+        while (in.hasNextLine()) { //this prints out each line
+            String line = in.nextLine();
+            numRows++; //keeps track of num of rows for array of chars
+            if (numRows == 1) { //counts the number of columns
+                for (int i = 0; i < line.length(); i++) {
+                    numCols++;
+                }
+            }
+        }
     }
 }
