8e642da1392b6ab373c6ff0f6566f2b11c523bdb
qtgeo1248 <gzhou00@stuy.edu>
2019-02-28 11:17:16 -0500

    tested tostring

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Driver {
     public static void main(String[] args) {
-        try {
+        try { //testing exceptions
             Maze test = new Maze("data1.data");
         } catch (FileNotFoundException e) {
             System.out.println("Exception Caught!\n");
@@ -18,6 +18,13 @@ public class Driver {
             Maze test = new Maze("wrong2.dat");
         } catch (IllegalStateException e) {
             System.out.println("Exception Caught!\n");
+        } catch (FileNotFoundException e) {
+            System.out.println("Wrong Exception Caught!\n");
+        }
+
+        try {
+            Maze test = new Maze("data1.dat");
+            System.out.println(test);
         } catch (FileNotFoundException e) {
             System.out.println("Wrong Exception Caught!\n");
         }
